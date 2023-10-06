ab620ca814636b35db3d102e448dafe3c0d49f7c
Jake Dalton <jda178@uclive.ac.nz>
2023-10-06 15:00:59 +1300

    Refactor helper funcs and bar handling into their own modules

---
+++ final/bar.c
@@ -0,0 +1,28 @@
+#include "navswitch.h"
+#include "helper.h"
+#include "bar.h"
+
+Vector_t position;
+
+void bar_init(bar_length) {
+    position = (Vector_t) {(LEDMAT_ROWS_NUM / 2) - (BAR_LENGTH / 2), LEDMAT_COLS_NUM - 1};
+}
+
+Vector_t bar_set_position(int8_t x, int8_t y) {
+    position.x = x;
+    position.y = y;
+}
+
+void bar_update() {
+    navswitch_update ();
+    if (navswitch_push_event_p(NAVSWITCH_NORTH)) { 
+        position.x = max(0, position.x - 1);
+    }
+    else if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
+        position.x = min(LEDMAT_COLS_NUM - BAR_LENGTH + 2, position.x + 1);
+    }
+}
+
+void bar_display() {
+    ledmat_display_column(BAR_MASK << position.x, position.y);
+}
---
+++ final/helper.c
@@ -0,0 +1,13 @@
+
+#include "system.h"
+#include "helper.h"
+
+int8_t max(int8_t op1, int8_t op2)
+{
+    return op1 > op2 ? op1 : op2;
+}
+
+int8_t min(int8_t op1, int8_t op2)
+{
+    return op1 > op2 ? op2 : op1;
+}
