a0ba6c75b7c62afe1c1d08906d93a54105a0c6cc
Zipporah Price <zpr12@uclive.ac.nz>
2023-10-17 15:49:33 +1300

    Adding logic for sending and updating scores

--- final/game.c
+++ final/game.c
@@ -17,6 +17,10 @@
 
 #define SCORE_PACKET 0
 #define BALL_PACKET 1
+#define WON 3
+
+int8_t this_score = 0;
+int8_t other_score = 0;
 
 
 void display_character (char character)
@@ -27,30 +31,6 @@ void display_character (char character)
     tinygl_text (buffer);
 }
 
-// int8_t generate_ball_packet(int8_t row_position, int8_t direction) {
-//     int8_t packet = 1;
-//     packet += (BALL_PACKET << 7);
-//     packet += (row_position << 4);
-//     packet += (((direction*-1)+1) << 2);
-//     return packet;
-// }
-
-// void parse_ball_packet(int8_t packet) {
-//     int8_t packet_type = (packet & (1 << 7)) >> 7;
-//     int8_t row_position = (packet >> 4) & 0b111;
-//     int8_t y_direction = ((packet >> 2) & 0b11) - 1; 
-
-//     switch (packet_type)
-//     {
-//         case SCORE_PACKET:
-//             break;
-        
-//         case BALL_PACKET:
-//             set_ball_position(row_position, 0);
-//             set_ball_velocity(y_direction, 1);
-//             break;
-//     }
-// }
 
 void send_ball_packet(int8_t row_position, int8_t direction) {
     ir_uart_putc(BALL_PACKET);
@@ -58,12 +38,29 @@ void send_ball_packet(int8_t row_position, int8_t direction) {
     ir_uart_putc(-1 * direction);
 }
 
+/**
+ * Sends the current scores to the other player
+*/
+void send_score_packet() {
+    ir_uart_putc(SCORE_PACKET);
+    ir_uart_putc(this_score);
+    ir_uart_putc(other_score);
+}
+
+/**
+ * Receives data: either ball position or score update
+*/
 void receive_packet(void) {
     int8_t packet_type = ir_uart_getc();
     switch (packet_type) {
         case (BALL_PACKET):
             set_ball_position(ir_uart_getc(), 0);
             set_ball_velocity(ir_uart_getc(), 1);
+            break;
+        
+        case (SCORE_PACKET):
+            other_score = ir_uart_getc();
+            this_score = ir_uart_getc();
             break;
     }
 }
@@ -80,6 +77,17 @@ void send(int8_t packet) {
         if (ir_uart_read_ready_p()) {
             packet_received = ir_uart_getc();
         }
+    }
+}
+
+void check_score()
+{
+    if (this_score == WON) {
+        // do won screen
+    } else if (other_score == WON) {
+        // do lost screen
+    } else {
+        // continue playing
     }
 }
 
@@ -154,7 +162,9 @@ int main (void)
             if (hits_paddle(bar_get_position())) {
                 paddle_bounce();
             }
+            // if it hits the back wall, send updated scores
             if (hits_back_wall()) {
+                send_score_packet();
                 wall_stop();
             } 
             if (get_ball_position().y < 0 && get_velocity().y == -1) {
